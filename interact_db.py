836f74023485180ff6d1016bab208a7c7ed321a2
Shruthi Reddy <shruthi6reddy@gmail.com>
2025-04-01 09:30:17 +0100

    updated tables and API calls

--- interact_db.py
+++ interact_db.py
@@ -1,16 +1,23 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, Mapped, mapped_column, relationship
+import datetime
+
+
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+Base = declarative_base()
 
-with engine.connect() as conn:
-    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
-    conn.execute(
-        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
-        [{"x": 1, "y": 1}, {"x": 2, "y": 4}],
-    )
-    conn.commit()
+class Employee(Base):
+    __tablename__ = "employee_table"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    employee_name = Column(String, nullable=False)
 
-with engine.begin() as conn:
-    conn.execute(
-        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
-        [{"x": 6, "y": 8}, {"x": 9, "y": 10}],
-    )
+
+class Expenses(Base):
+    __tablename__ = "expenses_table"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    employee_id = Column(Integer, ForeignKey("employee_table.id"))
+    amount = Column(Integer, nullable= False)
+    expense_type = Column(String, nullable=False)
+    created_time = Column(DateTime, default=datetime.datetime.now)
+
+# we could keep an expense type table for normalization
--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from typing import Union
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
+class Expenses(BaseModel):
+    expense_type: str
+    amount: float
     is_offer: Union[bool, None] = None
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
+@app.get("/add_employee")
+def add_new_employee(employee_name):
+    
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_price": item.price, "item_id": item_id}
+def update_item(item_id: int, expense: Expenses):
+    return {"expense_amount": expense.amount, "item_id": item_id}
